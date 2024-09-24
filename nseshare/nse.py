87ae79be8fd21a2ee7e0c04f8de13111dfe8291c
Kumaran <kumaranbecse@gmail.com>
2024-09-24 10:49:42 +0530

    NSE Stock Analysis

--- nseshare/nse.py
+++ nseshare/nse.py
@@ -11,5 +11,7 @@ result3 = capital_market.deliverable_position_data('SBIN',period='1M')
 print(result3)
 result4 = capital_market.price_volume_and_deliverable_position_data('INFY',period='1M')
 print(result4)
+result5 = capital_market.equity_list()
+print(result5)
 
 
