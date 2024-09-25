7f3a71a618f4d7f2dd69467d6eaf7a2b14461f7a
Kumaran <kumaranbecse@gmail.com>
2024-09-25 10:50:52 +0530

    NSE Stock Analysis

--- nseshare/Pavinsc.py
+++ nseshare/Pavinsc.py
@@ -6,9 +6,10 @@ from nselib import capital_market
 result = capital_market.nifty50_equity_list()
 print(result)
 list_of_stock = ['AXISBANK', 'TAPARIA', 'AAVAS', 'AADHARHFC', 'NMDC','IDFCFIRSTB']
-#for stock in list_of_stock:
-#    result = capital_market.price_volume_and_deliverable_position_data(stock, period='1D')
-#    print(stock,result)
+print ( capital_market.equity_list());
+for stock in list_of_stock:
+    result = capital_market.price_volume_and_deliverable_position_data(stock, period='1D')
+    print(stock,result)
 
 
 
