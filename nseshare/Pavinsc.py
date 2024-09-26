0c04b477490ddf36221bd3eeaf6043b546394931
Kumaran <kumaranbecse@gmail.com>
2024-09-26 19:36:44 +0530

    NSE Stock Analysis

--- nseshare/Pavinsc.py
+++ nseshare/Pavinsc.py
@@ -14,9 +14,11 @@ read_file = pd.read_excel("stocks.xlsx")
 
 list_of_stock = read_file.to_dict(orient='records')
 result = capital_market.week_52_high_low_report('25-09-2024')
-print(result)
+#print(result.columns.values)
+data = capital_market.var_end_of_day(trade_date='25-09-2024')
+#print(data.columns.values)
 for stock in list_of_stock:
     data = stock['Stock name']
-   # result = capital_market.price_volume_data(data, period='1D')
-
-    #print(result)
+    targetPrice =
+    result = capital_market.price_volume_data(data, period='1D')
+    print(data,result['ClosePrice'].values)
